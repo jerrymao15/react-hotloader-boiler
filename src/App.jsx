6ef9b77e0298feeebc4dce6d413eaf9d3b22cc13
jerrymao15 <jerryy.mao@gmail.com>
2016-06-29 12:03:32 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+'use strict';
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return (
+      <div>
+      <h1>This is a hot reload example</h1>
+      Hello World!
+      </div>
+    )
+  }
+}
+
+
+export default App;
